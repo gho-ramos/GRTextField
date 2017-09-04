f05f3b54fb51c9bae475037680cc5f9b66781bc0
Guilherme <gramos@Zup-Guilherme-MacBook-Pro.local>
2017-09-04 15:02:28 -0300

    Update travis file

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-set -o pipefail && xcodebuild test -enableCodeCoverage YES -workspace Example/GRTextField.xcworkspace -scheme GRTextField-Example -destination 'platform=iOS Simulator,name=iPhone 5,OS=10.0' | xcpretty
+set -o pipefail && xcodebuild test -enableCodeCoverage YES -workspace Example/GRTextField.xcworkspace -scheme GRTextField-Example -destination 'platform=iOS Simulator,name=iPhone 5,OS=10.3.1' | xcpretty
 pod lib lint
